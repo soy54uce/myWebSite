54f28c02b0cd88007ad197b5b329e846ce5d6810
Ben Michael Beaudin <bbeaudin@uvm.edu>
2018-11-03 20:59:21 -0400

    change panel IDs and add .gitignore

--- homepage.js
+++ homepage.js
@@ -4,7 +4,8 @@ var panels = document.getElementsByClassName('panelContent');
 document.write(panels.length);
 
 console.log(panels);
-
+var p1 = document.getElementById("panel1");
+console.log(p1);
 var i;
 
 for (i = 0;i < panels.length; i++) {
@@ -15,7 +16,7 @@ for (i = 0;i < panels.length; i++) {
 		console.log("mouseon");
 		panels[i].innerHTML = "hi";
 	});
-	
+
 	panels[i].addEventListener("mouseout", function() {
 		console.log("mouseout");
 		panels[i].innerHTML = "hi";
